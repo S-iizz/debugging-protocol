cfa5c060eff20a53af48b5e7fdb867f2c93a2197
S-iizz <sssshhhhseo@gmail.com>
2025-06-16 12:38:44 +0900

    Add falling.html bgm and update script

--- falling.js
+++ falling.js
@@ -1,5 +1,15 @@
 window.onload = () => {
+  const bgm = document.getElementById("falling-bgm");
+  bgm.volume = 0.7;
+
+  bgm.play().catch(err => {
+    console.warn("Falling BGM autoplay blocked:", err);
+  });
+
   setTimeout(() => {
+    bgm.pause();
+    bgm.currentTime = 0;
+
     window.location.href = "index.html?from=falling";
-  }, 15000);
+  }, 20000);
 };
